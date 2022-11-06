dd9815234f408f5e84e2c42c2cae5b9d1bfb3629
Gabriela Macias <gabrielamaciassolis@gmail.com>
2022-11-06 14:58:34 +0000

    change colors

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -80,10 +80,10 @@ const ProductScreen = ({ history, match }) => {
                   <h3>{product.name}</h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Rating
+                  {/* <Rating
                     value={product.rating}
                     text={`${product.numReviews} reviews`}
-                  />
+                  /> */}
                 </ListGroup.Item>
                 <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
                 <ListGroup.Item>
@@ -159,74 +159,6 @@ const ProductScreen = ({ history, match }) => {
                   </ListGroup.Item>
                 </ListGroup>
               </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={6}>
-              <h2>Opiniones</h2>
-              {/* {product.reviews.length === 0 && <Message>Sin Opiniones</Message>} */}
-              <ListGroup variant='flush'>
-                {product.reviews.map((review) => (
-                  <ListGroup.Item key={review._id}>
-                    <strong>{review.name}</strong>
-                    <Rating value={review.rating} />
-                    <p>{review.createdAt.substring(0, 10)}</p>
-                    <p>{review.comment}</p>
-                  </ListGroup.Item>
-                ))}
-                <ListGroup.Item>
-                  <h2>Danos tu opinión</h2>
-                  {successProductReview && (
-                    <Message variant='success'>
-                      Opinión enviada correctamente
-                    </Message>
-                  )}
-                  {loadingProductReview && <Loader />}
-                  {errorProductReview && (
-                    <Message variant='danger'>{errorProductReview}</Message>
-                  )}
-                  {userInfo ? (
-                    <Form onSubmit={submitHandler}>
-                      <Form.Group controlId='rating'>
-                        <Form.Label>Rating</Form.Label>
-                        <Form.Control
-                          as='select'
-                          value={rating}
-                          onChange={(e) => setRating(e.target.value)}
-                        >
-                          <option value=''>Select...</option>
-                          <option value='1'>1 - Pobre</option>
-                          <option value='2'>2 - Justo</option>
-                          <option value='3'>3 - Bien</option>
-                          <option value='4'>4 - Muy Bien</option>
-                          <option value='5'>5 - Excelente</option>
-                        </Form.Control>
-                      </Form.Group>
-                      <Form.Group controlId='comment'>
-                        <Form.Label>Comentario</Form.Label>
-                        <Form.Control
-                          as='textarea'
-                          row='3'
-                          value={comment}
-                          onChange={(e) => setComment(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-                      <Button
-                        disabled={loadingProductReview}
-                        type='submit'
-                        variant='primary'
-                      >
-                        Enviar
-                      </Button>
-                    </Form>
-                  ) : (
-                    <Message>
-                      Por favor <Link to='/login'>iniciar sesión</Link> para
-                      escribir una opinión{' '}
-                    </Message>
-                  )}
-                </ListGroup.Item>
-              </ListGroup>
             </Col>
           </Row>
         </>
